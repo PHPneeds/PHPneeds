be7b442cac1c5c9603701b2b8a68167fb778adbc
Mertcan Ayhan <mertowitch@gmail.com>
2021-09-28 22:55:33 +0300

    tab changed to 4 space
    Signed-off-by: Mertcan Ayhan <mertowitch@gmail.com>

--- libs/Redis.php
+++ libs/Redis.php
@@ -12,54 +12,54 @@
 namespace Mertowitch\Phpneeds
 {
 
-	class Redis extends \Redis
-	{
-		private static ?Redis $instance = null;
-		private static object $config;
+    class Redis extends \Redis
+    {
+        private static ?Redis $instance = null;
+        private static object $config;
 
-		private function __construct()
-		{
-			parent::__construct();
-			self::_getConfig();
-		}
+        private function __construct()
+        {
+            parent::__construct();
+            self::_getConfig();
+        }
 
-		private static function _getConfig(): void
-		{
-			self::$config = include( __DIR__ . '/../confs/conf.redis.php' );
-		}
+        private static function _getConfig(): void
+        {
+            self::$config = include( __DIR__ . '/../confs/conf.redis.php' );
+        }
 
-		public static function getInstance(): Redis
-		{
-			if ( self::$instance === null )
-			{
-				self::$instance = ( new self() )->_getNewInstance();
-			}
+        public static function getInstance(): Redis
+        {
+            if ( self::$instance === null )
+            {
+                self::$instance = ( new self() )->_getNewInstance();
+            }
 
-			return self::$instance;
-		}
+            return self::$instance;
+        }
 
-		private function _getNewInstance(): Redis
-		{
-			try
-			{
-				$newRedisInstance = new Redis();
+        private function _getNewInstance(): Redis
+        {
+            try
+            {
+                $newRedisInstance = new Redis();
 
-				$newRedisInstance->connect( self::$config->HOST, self::$config->PORT, 1, null, 0, 0 );
+                $newRedisInstance->connect( self::$config->HOST, self::$config->PORT, 1, null, 0, 0 );
 
-				$newRedisInstance->setOption( self::OPT_PREFIX, self::$config->PREFIX );
+                $newRedisInstance->setOption( self::OPT_PREFIX, self::$config->PREFIX );
 
-				if ( str_contains( session_save_path(), 'auth=' ) )
-				{
-					$newRedisInstance->auth( self::$config->PASS );
-				}
-			}
-			catch ( \Exception $e )
-			{
-				exit( $e->getMessage() );
-			}
+                if ( str_contains( session_save_path(), 'auth=' ) )
+                {
+                    $newRedisInstance->auth( self::$config->PASS );
+                }
+            }
+            catch ( \Exception $e )
+            {
+                exit( $e->getMessage() );
+            }
 
-			return $newRedisInstance;
-		}
+            return $newRedisInstance;
+        }
 
-	}
+    }
 }
--- libs/Session.php
+++ libs/Session.php
@@ -12,177 +12,177 @@
 namespace Mertowitch\Phpneeds
 {
 
-	use JsonException;
+    use JsonException;
 
-	/**
-	 * Class Session
-	 *
-	 * @package Mertowitch\Phpneeds
-	 */
-	class Session
-	{
-		private static object $config;
-		private static Redis $objRedis;
+    /**
+     * Class Session
+     *
+     * @package Mertowitch\Phpneeds
+     */
+    class Session
+    {
+        private static object $config;
+        private static Redis $objRedis;
 
-		/**
-		 * Session constructor.
-		 *
-		 * @param Redis $objRedis
-		 */
-		public function __construct( Redis $objRedis )
-		{
-			self::_getConfig();
-			self::$objRedis = $objRedis;
-		}
+        /**
+         * Session constructor.
+         *
+         * @param Redis $objRedis
+         */
+        public function __construct( Redis $objRedis )
+        {
+            self::_getConfig();
+            self::$objRedis = $objRedis;
+        }
 
-		private static function _getConfig(): void
-		{
-			self::$config = include( __DIR__ . '/../confs/conf.session.php' );
-		}
+        private static function _getConfig(): void
+        {
+            self::$config = include( __DIR__ . '/../confs/conf.session.php' );
+        }
 
-		/**
-		 * Destroy the session self like 'session_destroy()'
-		 *
-		 * @return bool
-		 */
-		public static function selfDestroy(): bool
-		{
-			return session_destroy();
-		}
+        /**
+         * Destroy the session self like 'session_destroy()'
+         *
+         * @return bool
+         */
+        public static function selfDestroy(): bool
+        {
+            return session_destroy();
+        }
 
-		/**
-		 * Initialize the session
-		 *
-		 * @return bool
-		 */
-		public function init(): bool
-		{
-			switch ( self::$config->MODE )
-			{
-				case 'files':
-					ini_set( 'session.save_handler', 'files' );
-					ini_set( 'session.name', self::$config->NAME );
-					ini_set( 'session.save_path', self::$config->PATH );
-					ini_set( "session.cookie_httponly", '1' );
-					ini_set( 'session.cookie_secure', '1' );
+        /**
+         * Initialize the session
+         *
+         * @return bool
+         */
+        public function init(): bool
+        {
+            switch ( self::$config->MODE )
+            {
+                case 'files':
+                    ini_set( 'session.save_handler', 'files' );
+                    ini_set( 'session.name', self::$config->NAME );
+                    ini_set( 'session.save_path', self::$config->PATH );
+                    ini_set( "session.cookie_httponly", '1' );
+                    ini_set( 'session.cookie_secure', '1' );
 
-					ini_set( 'session.gc_maxlifetime', '1800' );
-					ini_set( 'session.gc_probability', '50' );
-					ini_set( 'session.gc_divisor', '100' );
+                    ini_set( 'session.gc_maxlifetime', '1800' );
+                    ini_set( 'session.gc_probability', '50' );
+                    ini_set( 'session.gc_divisor', '100' );
 
-					break;
+                    break;
 
-				case 'redis':
-					$prefixGlue = ( str_contains( session_save_path(), '?' ) ) ? '&' : '?';
+                case 'redis':
+                    $prefixGlue = ( str_contains( session_save_path(), '?' ) ) ? '&' : '?';
 
-					$sessionSavePath = session_save_path() . $prefixGlue . 'prefix=' . self::$config->NAME . ':';
+                    $sessionSavePath = session_save_path() . $prefixGlue . 'prefix=' . self::$config->NAME . ':';
 
-					ini_set( 'session.save_handler', 'redis' );
-					ini_set( 'session.save_path', $sessionSavePath );
-					ini_set( 'session.name', self::$config->NAME );
-					ini_set( "session.cookie_httponly", '1' );
-					ini_set( 'session.cookie_secure', '1' );
+                    ini_set( 'session.save_handler', 'redis' );
+                    ini_set( 'session.save_path', $sessionSavePath );
+                    ini_set( 'session.name', self::$config->NAME );
+                    ini_set( "session.cookie_httponly", '1' );
+                    ini_set( 'session.cookie_secure', '1' );
 
-					break;
-			}
+                    break;
+            }
 
-			return session_start();
-		}
+            return session_start();
+        }
 
-		/**
-		 * Destroy another session with sessionID
-		 *
-		 * @param string $sessionID
-		 *
-		 * @return bool
-		 */
-		public function destroy( string $sessionID ): bool
-		{
-			return match ( self::$config->MODE )
-			{
-				'files' => unlink( self::$config->PATH . 'sess_' . $sessionID ),
-				'redis' => (bool) self::$objRedis->del( self::$config->NAME . ':' . $sessionID ),
-				default => false,
-			};
-		}
+        /**
+         * Destroy another session with sessionID
+         *
+         * @param string $sessionID
+         *
+         * @return bool
+         */
+        public function destroy( string $sessionID ): bool
+        {
+            return match ( self::$config->MODE )
+            {
+                'files' => unlink( self::$config->PATH . 'sess_' . $sessionID ),
+                'redis' => (bool) self::$objRedis->del( self::$config->NAME . ':' . $sessionID ),
+                default => false,
+            };
+        }
 
-		/**
-		 * @throws JsonException
-		 */
-		public function getByUserName( string $userName ): array
-		{
-			$ArrSession = array();
+        /**
+         * @throws JsonException
+         */
+        public function getByUserName( string $userName ): array
+        {
+            $ArrSession = array();
 
-			foreach ( $this->getList() as $session )
-			{
-				$strlen  = strlen( $userName );
-				$pattern = "UserName|s:$strlen:\"$userName\"";
+            foreach ( $this->getList() as $session )
+            {
+                $strlen  = strlen( $userName );
+                $pattern = "UserName|s:$strlen:\"$userName\"";
 
-				if ( str_contains( $session['data'], $pattern ) )
-				{
+                if ( str_contains( $session['data'], $pattern ) )
+                {
 //					var_dump($session);
-					$ArrSession[] = $session;
-				}
-			}
+                    $ArrSession[] = $session;
+                }
+            }
 
 //			$ArrSession[0]=$pattern;
-			return $ArrSession;
-		}
+            return $ArrSession;
+        }
 
-		/**
-		 * @throws JsonException
-		 */
-		public function getList( string $outputFormat = 'array' ): array
-		{
-			$result = array();
+        /**
+         * @throws JsonException
+         */
+        public function getList( string $outputFormat = 'array' ): array
+        {
+            $result = array();
 
-			switch ( self::$config->MODE )
-			{
-				case 'files':
+            switch ( self::$config->MODE )
+            {
+                case 'files':
 
-					foreach ( glob( self::$config->PATH . "sess_*" ) as $filename )
-					{
-						$result[] = array(
-							'id'   => explode( '_', basename( $filename ) )[1],
-							'name' => basename( $filename ),
-							'size' => filesize( $filename ),
-							'data' => file_get_contents( $filename )
-						);
-					}
+                    foreach ( glob( self::$config->PATH . "sess_*" ) as $filename )
+                    {
+                        $result[] = array(
+                            'id'   => explode( '_', basename( $filename ) )[1],
+                            'name' => basename( $filename ),
+                            'size' => filesize( $filename ),
+                            'data' => file_get_contents( $filename )
+                        );
+                    }
 
-					break;
+                    break;
 
-				case 'redis':
+                case 'redis':
 
-					$arrSessionNames = self::$objRedis->keys( self::$config->NAME . '*' ) ?: [];
+                    $arrSessionNames = self::$objRedis->keys( self::$config->NAME . '*' ) ?: [];
 
-					foreach ( $arrSessionNames as $sessionName )
-					{
-						$session = self::$objRedis->get( $sessionName );
+                    foreach ( $arrSessionNames as $sessionName )
+                    {
+                        $session = self::$objRedis->get( $sessionName );
 
-						$result[] = array(
-							'id'   => explode( ':', $sessionName )[1],
-							'name' => basename( $sessionName ),
-							'data' => $session
-						);
-					}
+                        $result[] = array(
+                            'id'   => explode( ':', $sessionName )[1],
+                            'name' => basename( $sessionName ),
+                            'data' => $session
+                        );
+                    }
 
-					break;
-			}
+                    break;
+            }
 
-			try
-			{
-				return match ( $outputFormat )
-				{
-					'array' => $result,
-					'json' => json_encode( $result, JSON_THROW_ON_ERROR | JSON_FORCE_OBJECT ),
-				};
-			}
-			catch ( JsonException $e )
-			{
-				throw new JsonException ( $e->getMessage() );
-			}
-		}
+            try
+            {
+                return match ( $outputFormat )
+                {
+                    'array' => $result,
+                    'json' => json_encode( $result, JSON_THROW_ON_ERROR | JSON_FORCE_OBJECT ),
+                };
+            }
+            catch ( JsonException $e )
+            {
+                throw new JsonException ( $e->getMessage() );
+            }
+        }
 
-	}
+    }
 }
