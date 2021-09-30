489756e39fe2aa812512a231bf9f152a8f9cdb2d
Mertcan Ayhan <mertowitch@gmail.com>
2021-09-30 16:56:37 +0300

    ~ some fixes
    Signed-off-by: Mertcan Ayhan <mertowitch@gmail.com>

--- tools/database/create_table_users.php
+++ tools/database/create_table_users.php
@@ -8,17 +8,17 @@ require_once __DIR__ . "/../../vendor/autoload.php";
 $objDatabase = Database::getInstance();
 // ********DATABASE*************************
 
-if ( ! $objDatabase->getSchema( 'USER' ) )
+if ( ! $schema = $objDatabase->getSchema( 'USER' ) )
 {
-    exit( "\n\n\033[31m\u{274c}  'users' not found in schema! \033[0m\n\n\n" );
+    exit( "\n\n\033[31m\u{274c}  'USER' not found in schema! \033[0m\n\n\n" );
 }
 
 if ( $objDatabase->createTable( 'USER' ) )
 {
     // **green** success message
-    echo "\n\n\033[32m\u{2713}  'users' table created successfuly! \033[0m\n\n\n";
+    echo "\n\n\033[32m\u{2713}  '{$schema->NAME}' table created successfuly! \033[0m\n\n\n";
 } else
 {
     // **red** error message
-    echo "\n\n\033[31m\u{274c}  'users' table could not created! \033[0m\n\n\n";
+    echo "\n\n\033[31m\u{274c}  '{$schema->NAME}' table could not created! \033[0m\n\n\n";
 }
