9806ff21c66acc1017a5b7cfe4c18b1ed6c71269
Mertcan Ayhan <mertowitch@gmail.com>
2021-09-30 16:41:40 +0300

    + create_user_admin.php added
    Signed-off-by: Mertcan Ayhan <mertowitch@gmail.com>

---
+++ tools/database/create_user_admin.php
@@ -0,0 +1,29 @@
+<?php declare( strict_types=1 );
+
+namespace Mertowitch\Phpneeds;
+
+require_once __DIR__ . "/../../vendor/autoload.php";
+
+// ********DATABASE*************************
+$objDatabase = Database::getInstance();
+// ********DATABASE*************************
+
+// ********USER*****************************
+$objUser = new User( $objDatabase );
+// ********DATABASE*************************
+
+$newPassword = User::getNewRandomPassword( 16 );
+
+$md5Password = md5( $newPassword );
+
+if ( $objUser->createUser( 'admin', $md5Password ) )
+{
+    // **green** success message
+    echo "\n\n\033[32m\u{2713}  User 'admin' created successfuly! \033[0m\n";
+    echo "\n\033[32m  Username: admin \033[0m\n";
+    echo "\n\033[32m  Password: $newPassword \033[0m\n\n\n";
+} else
+{
+    // **red** error message
+    echo "\n\n\033[31m\u{274c}  User could not created! \033[0m\n\n\n";
+}
